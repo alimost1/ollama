bcfcc1499294b451ecb8aef4562073687e509c08
Mohamed ali Khallal <39303616+alimost1@users.noreply.github.com>
2025-01-29 15:00:21 +0100

    Create entrypoint.sh
    bach

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Starting Ollama server..."
+ollama serve &
+SERVE_PID=$!
+
+echo "Waiting for Ollama server to be active..."
+while ! ollama list | grep -q 'NAME'; do
+  sleep 1
+done
+
+
+ollama run deepseek-r1:1.5b
+
+wait $SERVE_PID
